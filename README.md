9e2f1cbd3fa166611b6dceb3a34a228db32ae7b0
Karsten <karsten.sitterberg@googlemail.com>
2023-06-23 22:45:17 +0200

    feat: Initial component-only version

--- README.md
+++ README.md
@@ -1,27 +1,15 @@
-# WasmCam
+# Wasm Background-Blur with Angular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.1.
+Example application using Tensorflow light with wasm and wasm-simd to blur the background of a videostream. Videostream is gathered using `navigator.mediaDevices.getUserMedia()`. 
+Prove of concet to showcase Wasm capabilities within an angular application.
 
-## Development server
+## How to run
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+Run the project using either Angular-CLI with `ng serve` or using NPM with `npm run start`.
+You also need to run the app on `localhost` or via `https` to be able to access `navigator.mediaDevices`.
 
-## Code scaffolding
+## Feel free to play around
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+If you need help with your angular project, feel free to reach out. \
+You can find me here:
+https://sitterberg.com/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, ElementRef, ViewChild } from '@angular/core';
+import { simd } from 'wasm-feature-detect';
+
+declare var createTFLiteModule: Function;
+declare var createTFLiteSIMDModule: Function;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,174 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'wasm-cam';
+
+  @ViewChild('videoEl') video!: ElementRef<HTMLVideoElement>;
+  @ViewChild('canvasEl') canvas!: ElementRef<HTMLCanvasElement>;
+
+  private readonly options = {
+    height: 144,
+    width: 256,
+    blurValue: 15
+  };
+  private readonly segmentationPixelCount = this.options.width * this.options.height;
+
+  private tflite: any;
+  private animationFrame?: number;
+
+  private segmentationMask!: ImageData;
+  private segmentationMaskCanvas!: HTMLCanvasElement;
+  private segmentationMaskCtx!: CanvasRenderingContext2D | null;
+  private outputCanvasContext?: CanvasRenderingContext2D | null;
+  private mediaStream?: MediaStream;
+
+  constructor(destroy: DestroyRef) {
+    destroy.onDestroy(() => this.cancelCanvas());
+    simd().then(issimd => {
+      const script = document.createElement('script');
+      if (issimd) {
+        script.src = 'assets/tflite/tflite-simd.js';
+      } else {
+        script.src = 'assets/tflite/tflite.js';
+      }
+      document.body.append(script);
+      script.onload = () => this.init(issimd ? createTFLiteSIMDModule : createTFLiteModule);
+    });
+  }
+
+  async init(createModule: Function): Promise<void> {
+    this.tflite = await createModule();
+    let modelBuffer: ArrayBuffer;
+
+    const modelResponse = await fetch('/assets/tflite/selfie_segmentation_landscape.tflite');
+
+    if (!modelResponse.ok) {
+      throw new Error('Failed to download tflite model!');
+    }
+
+    modelBuffer = await modelResponse.arrayBuffer();
+
+    this.tflite.HEAPU8.set(new Uint8Array(modelBuffer), this.tflite._getModelBufferMemoryOffset());
+    this.tflite._loadModel(modelBuffer.byteLength);
+
+    this.segmentationMask = new ImageData(this.options.width, this.options.height);
+    this.segmentationMaskCanvas = document.createElement('canvas');
+    this.segmentationMaskCanvas.width = this.options.width;
+    this.segmentationMaskCanvas.height = this.options.height;
+    this.segmentationMaskCtx = this.segmentationMaskCanvas.getContext('2d');
+  }
+
+  startCam() {
+    this.cancelCanvas();
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {facingMode: 'user', width: 640, height: 360}
+      })
+      .then((mediaStream) => {
+        this.video.nativeElement.srcObject = mediaStream;
+        this.mediaStream = mediaStream;
+        this.video.nativeElement.onloadedmetadata = () => {
+          this.video.nativeElement.play();
+        };
+        this.outputCanvasContext = this.canvas.nativeElement.getContext('2d');
+        this.startCanvas();
+      });
+  }
+
+  stopCam(): void {
+    this.video.nativeElement.remove();
+    this.canvas.nativeElement.remove();
+    this.mediaStream?.getTracks().forEach(track => track.stop());
+    this.cancelCanvas();
+  }
+
+  private resizeSource() {
+    this.segmentationMaskCtx?.drawImage(
+      this.video.nativeElement,
+      0,
+      0,
+      this.video.nativeElement.videoWidth,
+      this.video.nativeElement.videoHeight,
+      0,
+      0,
+      this.options.width,
+      this.options.height
+    );
+
+    const imageData = this.segmentationMaskCtx?.getImageData(
+      0,
+      0,
+      this.options.width,
+      this.options.height
+    );
+    const inputMemoryOffset = this.tflite._getInputMemoryOffset() / 4;
+
+    for (let i = 0; i < this.segmentationPixelCount; i++) {
+      this.tflite.HEAPF32[inputMemoryOffset + (i * 3)] = Number(imageData?.data[i * 4]) / 255;
+      this.tflite.HEAPF32[inputMemoryOffset + (i * 3) + 1] = Number(imageData?.data[(i * 4) + 1]) / 255;
+      this.tflite.HEAPF32[inputMemoryOffset + (i * 3) + 2] = Number(imageData?.data[(i * 4) + 2]) / 255;
+    }
+  }
+
+  private findPerson() {
+    this.tflite._runInference();
+    const outputMemoryOffset = this.tflite._getOutputMemoryOffset() / 4;
+
+    for (let i = 0; i < this.segmentationPixelCount; i++) {
+      const person = this.tflite.HEAPF32[outputMemoryOffset + i];
+
+      // Sets only the alpha component of each pixel.
+      this.segmentationMask.data[(i * 4) + 3] = 255 * person;
+    }
+    this.segmentationMaskCtx?.putImageData(this.segmentationMask, 0, 0);
+  }
+
+  private runPostProcessing() {
+    if (!this.outputCanvasContext) {
+      return;
+    }
+
+    const height = this.video.nativeElement.videoHeight;
+    const width = this.video.nativeElement.videoWidth;
+    this.canvas.nativeElement.height = height;
+    this.canvas.nativeElement.width = width;
+    this.outputCanvasContext.globalCompositeOperation = 'copy';
+
+    this.outputCanvasContext.filter = 'blur(4px)';
+    this.outputCanvasContext?.drawImage(
+      this.segmentationMaskCanvas,
+      0,
+      0,
+      this.options.width,
+      this.options.height,
+      0,
+      0,
+      this.video.nativeElement.videoWidth,
+      this.video.nativeElement.videoHeight
+    );
+    this.outputCanvasContext.globalCompositeOperation = 'source-in';
+    this.outputCanvasContext.filter = 'none';
+
+    // Draw the foreground video.
+    this.outputCanvasContext?.drawImage(this.video.nativeElement, 0, 0);
+
+    // Draw the background.
+    this.outputCanvasContext.globalCompositeOperation = 'destination-over';
+    this.outputCanvasContext.filter = `blur(${this.options.blurValue}px)`;
+    this.outputCanvasContext?.drawImage(this.video.nativeElement, 0, 0);
+  }
+
+  private startCanvas(): void {
+    this.animationFrame = requestAnimationFrame(() => {
+      this.resizeSource();
+      this.findPerson();
+      this.runPostProcessing();
+      this.startCanvas();
+    });
+  }
+
+  private cancelCanvas(): void {
+    if (this.animationFrame) {
+      cancelAnimationFrame(this.animationFrame);
+    }
+  }
 }
